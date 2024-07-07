8c76179a440d4138d28be2cf4c74e8dc1a87da71
Jayasekara Kapukotuwa <ncbdrck@gmail.com>
2024-07-07 13:36:29 +0100

    updated

--- src/rl_training_validation/utils/multi_task_env.py
+++ src/rl_training_validation/utils/multi_task_env.py
@@ -4,7 +4,6 @@ from typing import Optional, Union
 from gymnasium import spaces
 import numpy as np
 
-# wrappers
 from multiros.wrappers.normalize_action_wrapper import NormalizeActionWrapper
 from multiros.wrappers.normalize_obs_wrapper import NormalizeObservationWrapper
 from multiros.wrappers.time_limit_wrapper import TimeLimitWrapper
@@ -18,8 +17,8 @@ class MultiTaskEnv(gym.Env):
     - Not tested with other environments
     """
 
-    def __init__(self, env_list: list[str], env_args_dict: Optional[dict] = None , wrapper_list: Optional[list] = None,
-                 wrapper_args_dict: Optional[dict] = None):
+    def __init__(self, env_list: list[str], env_args_dict: Optional[dict] = None,
+                 wrapper_list: Optional[list] = None, wrapper_args_dict: Optional[dict] = None):
         """
         Args:
             env_list: list of environment names - must be registered in gym
@@ -29,15 +28,26 @@ class MultiTaskEnv(gym.Env):
         """
         super(MultiTaskEnv, self).__init__()
 
-        # create the environments
+        # Initialize optional dictionaries
+        if env_args_dict is None:
+            env_args_dict = {}
+        if wrapper_args_dict is None:
+            wrapper_args_dict = {}
+
+        # Create the environments
         self.env_list = []
         for env_name in env_list:
             env = gym.make(env_name, **env_args_dict[env_name])
             self.env_list.append(env)
 
-        # apply wrappers
+        # Apply wrappers
         if wrapper_list is not None:
-            for env in self.env_list:
+            for i in range(len(self.env_list)):
+
+                # Get the environment
+                env = self.env_list[i]
+
+                # Apply the wrappers
                 for wrapper_name in wrapper_list:
                     if wrapper_name == 'NormalizeActionWrapper':
                         env = NormalizeActionWrapper(env)
@@ -47,6 +57,9 @@ class MultiTaskEnv(gym.Env):
                         env = TimeLimitWrapper(env, **wrapper_args_dict[wrapper_name])
                     else:
                         raise ValueError(f"Wrapper {wrapper_name} not implemented")
+
+                # Update the environment in the list
+                self.env_list[i] = env
 
         # Find maximum observation and action space dimensions
         max_obs_dim = max(env.observation_space.shape[0] for env in self.env_list)
@@ -58,37 +71,37 @@ class MultiTaskEnv(gym.Env):
 
         self.current_env = None
 
-
     def step(self, action):
         """
         Step through the environment with the given action
         """
-        # unpad the action
+        # Unpad the action
         action = self._unpad_action(action)
 
-        # step through the environment
+        # Step through the environment
         observation, reward, terminated, truncated, info = self.current_env.step(action)
 
-        # pad the observation to match the maximum observation space dimension
+        # Pad the observation to match the maximum observation space dimension
         observation = self._pad_obs(observation)
 
         return observation, reward, terminated, truncated, info
 
     def reset(self, **kwargs):
         """
-        Here we reset the old environment and find the next environment to train on.
+        Reset the old environment and find the next environment to train on.
         """
-        # reset the old environment to stop the episode
-        self.current_env.rest()
+        # Reset the old environment
+        if self.current_env is not None:
+            self.current_env.reset()  # stop the current environment. we don't need to close it since we may choose it again
 
-        # randomly select a new environment from the list
+        # Randomly select a new environment from the list
         rand_choice = np.random.choice(len(self.env_list))
         self.current_env = self.env_list[rand_choice]
 
-        # reset the new environment
+        # Reset the new environment
         obs, _ = self.current_env.reset(**kwargs)
 
-        # pad the observation to match the maximum observation space dimension
+        # Pad the observation to match the maximum observation space dimension
         padded_obs = self._pad_obs(obs)
 
         return padded_obs
@@ -101,7 +114,6 @@ class MultiTaskEnv(gym.Env):
         """
         Pad the observation with zeros to match the maximum observation space dimension
         """
-
         padded_obs = np.zeros(self.observation_space.shape)
         padded_obs[:obs.shape[0]] = obs
         return padded_obs
